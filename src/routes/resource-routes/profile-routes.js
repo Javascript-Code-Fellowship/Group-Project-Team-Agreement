4ace3bb5402f6f92fd67f18ae055f772661cedda
Rachnicrice <46403371+Rachnicrice@users.noreply.github.com>
2021-10-20 15:32:38 -0400

    added ability to query for specific profile

--- src/routes/resource-routes/profile-routes.js
+++ src/routes/resource-routes/profile-routes.js
@@ -22,7 +22,7 @@ const handleCreate = async (req, res, next) => {
 
 const handleGetOne = async (req, res, next) => {
   try {
-    let id = req.user.id;
+    let id = req.params.id;
     let record = await Profile.findOne({ where: { UserId: id } });
     res.status(200).send(record);
   } catch (e) {
@@ -60,7 +60,7 @@ const handleDeleteOne = async (req, res, next) => {
 };
 
 profileRouter.post('/profile', bearerAuth, acl('create'), handleCreate);
-profileRouter.get('/profile', bearerAuth, acl('read'), handleGetOne);
+profileRouter.get('/profile/:id', bearerAuth, acl('read'), handleGetOne);
 profileRouter.put('/profile', bearerAuth, acl('update'), handleUpdateOne);
 profileRouter.delete('/profile', bearerAuth, acl('delete'), handleDeleteOne);
 
